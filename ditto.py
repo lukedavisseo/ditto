6424b7735e8e6f366d5b99bde8e6b6c41a45c018
Luke Davis <92871067+lukedavisseo@users.noreply.github.com>
2024-10-14 16:24:40 +0100

    Update ditto.py

--- ditto.py
+++ ditto.py
@@ -1,16 +1,25 @@
 import streamlit as st
+import requests
 import bs4
 from bs4 import BeautifulSoup
 import streamlit.components.v1 as components
 
 st.header("DITTO — Duplicating Information To Transform Optimisations")
 
+def get_soup(url):
+	res = requests.get(url)
+	soup = BeautifulSoup(res.content, 'html.parser')
+	return soup
+
 def remove_tags(soup):
     for data in soup(['style', 'script']):
         data.decompose()
-    ps = soup.find_all(['h1', 'h2', 'h3', 'p', 'li'])
+    ps = soup.find_all(['h1', 'h2', 'h3', 'p', 'li', 'table'])
     for p in ps:
-    	p.span.unwrap()
+    	try:
+    		p.span.unwrap()
+    	except:
+    		continue
     return soup
 
 def remove_attrs(soup):
@@ -25,12 +34,19 @@ def remove_attrs(soup):
 	                     if key not in REMOVE_ATTRIBUTES}
 	return soup
 
+webpage = st.text_input("Enter a webpage to extract")
 html_file = st.file_uploader("Upload your HTML file", type="html", accept_multiple_files=False)
+submit = st.button("Submit")
 
-if html_file:
+if html_file and submit:
 	with html_file as h:
 		content = h.read()
 		soup = BeautifulSoup(content, 'html.parser')
 		v_soup = remove_tags(soup)
 		vg_soup = remove_attrs(v_soup)
 		st.html(str(vg_soup))
+elif webpage and submit:
+	soup = get_soup(webpage)
+	v_soup = remove_tags(soup)
+	vg_soup = remove_attrs(v_soup)
+	st.html(str(vg_soup))
